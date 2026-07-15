90e707666b7cc2da97c002d4dd8e7c9c6b09fab8
coderforlife <83534631+avishimaheshwari1@users.noreply.github.com>
2026-07-15 11:48:12 +0530

    Add files via upload

--- largestdigit.c
+++ largestdigit.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
     scanf("%lld",&a);
 
     int digit;
-    int largestdigit;
+    int smallestdigit;
     long long int digits;
     copy1=a;
     copy2=a;
@@ -19,26 +19,26 @@ int main(int argc, char const *argv[])
         copy1=copy1/10;
     } while (copy1>0);
     
-    largestdigit=copy2%10;
+    smallestdigit=copy2%10;
 
     for (int i =1; i < digits; i++)
     {
         copy2=copy2/10;
          digit=copy2%10;
 
-        if (largestdigit>digit)
+        if (smallestdigit<digit)
         {
-            largestdigit=largestdigit;
+            smallestdigit=smallestdigit;
         }
         else{
-            largestdigit=digit;
+            smallestdigit=digit;
         }
         
         
 
     }
     
-    printf("%d",largestdigit);
+    printf("%d",smallestdigit);
 
 
     return 0;
