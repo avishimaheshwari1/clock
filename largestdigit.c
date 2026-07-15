250a513270b6ddf2328318d2c17ae5bc85c08c6b
coderforlife <83534631+avishimaheshwari1@users.noreply.github.com>
2026-07-15 11:50:13 +0530

    Change smallest digit logic to largest digit logic

--- largestdigit.c
+++ largestdigit.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
     scanf("%lld",&a);
 
     int digit;
-    int smallestdigit;
+    int largestdigit;
     long long int digits;
     copy1=a;
     copy2=a;
@@ -19,26 +19,26 @@ int main(int argc, char const *argv[])
         copy1=copy1/10;
     } while (copy1>0);
     
-    smallestdigit=copy2%10;
+    largestdigit=copy2%10;
 
     for (int i =1; i < digits; i++)
     {
         copy2=copy2/10;
          digit=copy2%10;
 
-        if (smallestdigit<digit)
+        if (largestdigit>digit)
         {
-            smallestdigit=smallestdigit;
+            largesttdigit=largestdigit;
         }
         else{
-            smallestdigit=digit;
+            largestdigit=digit;
         }
         
         
 
     }
     
-    printf("%d",smallestdigit);
+    printf("%d",largestdigit);
 
 
     return 0;
